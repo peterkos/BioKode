14a9b412da17ad465b5b5173de842b447033a6e5
Peter Kos <pkos91@icloud.com>
2016-04-21 20:09:32 -0400

    :wave: semicolons

--- BioKode/AppDelegate.swift
+++ BioKode/AppDelegate.swift
@@ -42,13 +42,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var mBothButton: NSButton!
     @IBAction func translationMode(sender: NSButton) {
         if (mRNAButton.state == 1) {
-            convertInput_mRNA(input: inputStr, output: outputStr);
+            convertInput_mRNA(input: inputStr, output: outputStr)
         } else if (mEnglishButton.state == 1) {
-            convertInput_mEnglish(input: inputStr, output: outputStr);
+            convertInput_mEnglish(input: inputStr, output: outputStr)
         } else {
-            let mRNAIntermediateOutput = (NSTextField)();
-            convertInput_mRNA(input: inputStr, output: mRNAIntermediateOutput);
-            convertInput_mEnglish(input: mRNAIntermediateOutput, output: outputStr);
+            let mRNAIntermediateOutput = (NSTextField)()
+            convertInput_mRNA(input: inputStr, output: mRNAIntermediateOutput)
+            convertInput_mEnglish(input: mRNAIntermediateOutput, output: outputStr)
         }
     }
     
@@ -59,21 +59,21 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func convertInput_mRNA(input input: NSTextField, output: NSTextField) {
         
         // Resets output
-        output.stringValue = "";
+        output.stringValue = ""
         
         // Gets input text
-        let textIn = String(input.stringValue);
+        let textIn = String(input.stringValue)
         
         // Converts to mRNA
         for i in textIn.characters {
             if (i == "T") {
-                output.stringValue += "A";
+                output.stringValue += "A"
             } else if (i == "C") {
-                output.stringValue += "G";
+                output.stringValue += "G"
             } else if (i == "G") {
-                output.stringValue += "C";
+                output.stringValue += "C"
             } else if (i == "A") {
-                output.stringValue += "U";
+                output.stringValue += "U"
             }
         }
     }
@@ -83,71 +83,71 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         output.stringValue = "";
         
         // Gets input text
-        let textIn = String(input.stringValue);
-        var preEnglish = [String]();
+        let textIn = String(input.stringValue)
+        var preEnglish = [String]()
         
         // Converts to codons
         for i in 0.stride(to: textIn.characters.count, by: 3) {
-            preEnglish.append(textIn[i..<(i + 3)]);
+            preEnglish.append(textIn[i..<(i + 3)])
         }
         
         // Creates final array, converts to English
-        var inEnglish = [String]();
+        var inEnglish = [String]()
         for str in preEnglish {
             switch str {
                 // U *****************************
                 // UU
-                case "UUU", "UAC":               inEnglish.append("A");
-                case "UUA", "UUG":               inEnglish.append("Q");
+                case "UUU", "UAC":               inEnglish.append("A")
+                case "UUA", "UUG":               inEnglish.append("Q")
                 // UC
-                case "UCU", "UCC", "UCA", "UCG": inEnglish.append("S");
+                case "UCU", "UCC", "UCA", "UCG": inEnglish.append("S")
                 // UA
-                case "UAU", "UAC":               inEnglish.append("C");
-                case "UAA", "UAG":               inEnglish.append("G");
+                case "UAU", "UAC":               inEnglish.append("C")
+                case "UAA", "UAG":               inEnglish.append("G")
 
                 // UG
-                case "UGU", "UCG":               inEnglish.append("H");
-                case "UGA":                      inEnglish.append("G");
-                case "UGG":                      inEnglish.append("W");
+                case "UGU", "UCG":               inEnglish.append("H")
+                case "UGA":                      inEnglish.append("G")
+                case "UGG":                      inEnglish.append("W")
                     
                 // C *****************************
                 // CU
-                case "CUU", "CUC", "CUA", "CUG": inEnglish.append("N");
+                case "CUU", "CUC", "CUA", "CUG": inEnglish.append("N")
                 // CC
-                case "CCU", "CCC", "CCA", "CCG": inEnglish.append("R");
+                case "CCU", "CCC", "CCA", "CCG": inEnglish.append("R")
                 // CA
-                case "CAU", "CAC":               inEnglish.append("L");
-                case "CAA", "CAG":               inEnglish.append("I");
+                case "CAU", "CAC":               inEnglish.append("L")
+                case "CAA", "CAG":               inEnglish.append("I")
                 // CG
-                case "CGU", "CGC", "CGA", "CGG": inEnglish.append("D");
+                case "CGU", "CGC", "CGA", "CGG": inEnglish.append("D")
                     
                 // A *****************************
                 // AU
-                case "AUU", "AUC", "AUA":        inEnglish.append("M");
-                case "AUG":                      inEnglish.append("P");
+                case "AUU", "AUC", "AUA":        inEnglish.append("M")
+                case "AUG":                      inEnglish.append("P")
                 // AC
-                case "ACU", "ACC":               inEnglish.append("V");
-                case "ACA", "ACG":               inEnglish.append("T");
+                case "ACU", "ACC":               inEnglish.append("V")
+                case "ACA", "ACG":               inEnglish.append("T")
                 // AA
-                case "AAU", "AAC":               inEnglish.append("E");
-                case "AAA", "AAG":               inEnglish.append("O");
+                case "AAU", "AAC":               inEnglish.append("E")
+                case "AAA", "AAG":               inEnglish.append("O")
                 // AG
-                case "AGU", "AGC":               inEnglish.append("S");
-                case "AGA", "AGG":               inEnglish.append("D");
+                case "AGU", "AGC":               inEnglish.append("S")
+                case "AGA", "AGG":               inEnglish.append("D")
                     
                 // G *****************************
                 // GU
-                case "GUU", "GUC":               inEnglish.append("Z");
-                case "GUA", "GUG":               inEnglish.append("Y");
+                case "GUU", "GUC":               inEnglish.append("Z")
+                case "GUA", "GUG":               inEnglish.append("Y")
                 // GC
-                case "GCU", "GCC":               inEnglish.append("U");
-                case "GCA", "GCG":               inEnglish.append("B");
+                case "GCU", "GCC":               inEnglish.append("U")
+                case "GCA", "GCG":               inEnglish.append("B")
                 // GA
-                case "GAU", "GAC":               inEnglish.append("F");
-                case "GAA", "GAG":               inEnglish.append("J");
+                case "GAU", "GAC":               inEnglish.append("F")
+                case "GAA", "GAG":               inEnglish.append("J")
                 // GG
-                case "GGU", "GGC", "GGA", "GGG": inEnglish.append("K");
-                default   : inEnglish.append("$");
+                case "GGU", "GGC", "GGA", "GGG": inEnglish.append("K")
+                default   : inEnglish.append("$")
             }
         }
         
