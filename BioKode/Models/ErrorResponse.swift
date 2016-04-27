90e5167cebf4143e5bca34d6012ab045a2bb9b39
Peter Kos <pkos91@icloud.com>
2016-04-26 21:10:35 -0400

    Condensed error checking functions

--- BioKode/Models/ErrorResponse.swift
+++ BioKode/Models/ErrorResponse.swift
@@ -14,60 +14,17 @@ class ErrorResponse {
 	var outputTextField : NSTextField
     var inputSegments  : NSSegmentedControl
 	var outputSegments : NSSegmentedControl
+	var alert = NSAlert()
     
 	init(inputTextField: NSTextField, outputTextField: NSTextField, inputSegments: NSSegmentedControl, outputSegments: NSSegmentedControl) {
         self.inputTextField = inputTextField
 		self.outputTextField = outputTextField
         self.inputSegments = inputSegments
 		self.outputSegments = outputSegments
-
-    }
-    
-    
-    func invalidDNASequence() {
-        let alert = NSAlert()
-        alert.messageText = "Uh oh!"
-        alert.informativeText = "Invalid sequence. Must contain A, C, T, or G, and the total length must be a multiple of 3."
-        alert.addButtonWithTitle("OK")
-        alert.addButtonWithTitle("Clear Input")
-		alert.addButtonWithTitle("Clear Input & Output")
-        let alertResponse = alert.runModal()
-        
-        // Reset the input field if the "Clear Input" button is selected
-        if (alertResponse == NSAlertSecondButtonReturn) {
-            inputTextField.stringValue = ""
-		} else if (alertResponse == NSAlertThirdButtonReturn) {
-			inputTextField.stringValue = ""
-			outputTextField.stringValue = ""
-			outputSegments.setSelected(false, forSegment: outputSegments.selectedSegment)
-		}
-        
-    }
-    
-    func invalidmRNASequence() {
-        let alert = NSAlert()
-        alert.messageText = "Uh oh!"
-        alert.informativeText = "Invalid sequence. Must contain A, C, U, or G, and the total length must be a multiple of 3."
-        alert.addButtonWithTitle("OK")
-        alert.addButtonWithTitle("Clear Input")
-		alert.addButtonWithTitle("Clear Input & Output")
-        let alertResponse = alert.runModal()
-        
-        // Reset the input field if the "Clear Input" button is selected
-        if (alertResponse == NSAlertSecondButtonReturn) {
-            inputTextField.stringValue = ""
-		} else if (alertResponse == NSAlertThirdButtonReturn) {
-			inputTextField.stringValue = ""
-			outputTextField.stringValue = ""
-			outputSegments.setSelected(false, forSegment: outputSegments.selectedSegment)
-		}
 		
     }
 	
-	func invalidEmptyInput() {
-		let alert = NSAlert()
-		alert.messageText = "Convert whitespace? That's a new one."
-		alert.informativeText = "Input string cannot be empty."
+	func alertButtonsAndActions() {
 		alert.addButtonWithTitle("OK")
 		alert.addButtonWithTitle("Clear Input")
 		alert.addButtonWithTitle("Clear Input & Output")
@@ -81,6 +38,26 @@ class ErrorResponse {
 			outputTextField.stringValue = ""
 			outputSegments.setSelected(false, forSegment: outputSegments.selectedSegment)
 		}
+	}
+	
+    func invalidDNASequence() {
+        alert.messageText = "Uh oh!"
+        alert.informativeText = "Invalid sequence. Must contain A, C, T, or G, and the total length must be a multiple of 3."
+		alertButtonsAndActions()
+        
+    }
+    
+    func invalidmRNASequence() {
+        alert.messageText = "Uh oh!"
+        alert.informativeText = "Invalid sequence. Must contain A, C, U, or G, and the total length must be a multiple of 3."
+        alertButtonsAndActions()
+		
+    }
+	
+	func invalidEmptyInput() {
+		alert.messageText = "Convert whitespace? That's a new one."
+		alert.informativeText = "Input string cannot be empty."
+		alertButtonsAndActions()
 	}
 	
 	
